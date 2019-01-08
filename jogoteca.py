5c7b46e20f788f3a774f52787a9d92e7d03f9f8a
gsardas <gsardas@gmail.com>
2019-01-08 16:40:10 -0200

    Add files via upload

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -10,13 +10,13 @@ class Jogo:
 
 jogo1 = Jogo('Super Mario', 'Acao', 'SNES')
 jogo2 = Jogo('Pokemon', 'RPG', 'GRA')
-jogo3 = Jogo('Mortal Kom', 'RPG', 'GBA')
+jogo3 = Jogo('Mortal Kom', 'RPG', 'Xbox')
 jogo4 = Jogo('Xadres', 'Tabuleiro', 'PC')
 lista = [jogo1, jogo2, jogo3, jogo4]
 
 
-@app.route('/inicio')
-def ola():
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Lista de Jogos', jogos=lista)
 
 
@@ -24,14 +24,16 @@ def ola():
 def novo():
     return render_template('novo.html', titulo='Novo Jogo')
 
-@app.route('/criar')
+@app.route('/criar', methods=['POST',])
 def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
     jogo = Jogo(nome,categoria,console)
     lista.append(jogo)
-    return render_template('lista.html', titulo='Lista de Jogos', jogos=lista)
+    return redirect('/')
 
-app.run()
+app.run(debug=True)
+
+
 
