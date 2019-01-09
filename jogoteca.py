c90e1c766a3482146a9af3f4249825d0e514948b
gsardas <gsardas@gmail.com>
2019-01-09 16:33:37 -0200

    Add files via upload

--- jogoteca.py
+++ jogoteca.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 app = Flask(__name__)
+
+app.secret_key = 'alura'
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -32,6 +34,22 @@ def criar():
     jogo = Jogo(nome,categoria,console)
     lista.append(jogo)
     return redirect('/')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'mestra' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(request.form['usuario'] + ' logou com sucesso!!')
+        return redirect('/')
+    else:
+        flash('Não logado, tente novamente!')
+        return redirect('/login')
+
+
 
 app.run(debug=True)
 
